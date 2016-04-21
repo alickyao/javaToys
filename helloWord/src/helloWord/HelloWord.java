3533f230a04422acded61d0b4e60b0ee4ea09567
alickyao <7181210@qq.com>
2016-04-21 11:08:18 +0800

    new

---
+++ helloWord/src/helloWord/HelloWord.java
@@ -0,0 +1,13 @@
+package helloWord;
+
+public class HelloWord {
+
+	public static void main(String[] agrs){
+		System.out.println("HelloWord");
+	}
+}
+
+//不同的项目可以有名字相同的class
+class TestExcaptionClass{
+	
+}
